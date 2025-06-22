ec290f70cd352e0cc8ca379c6919d23fba1f7107
NguyenTienAn1912 <sherlockh050@gmail.com>
2025-06-23 06:37:37 +0700

    updated

--- src/main/java/org/example/test01/Utils/MyUtils.java
+++ src/main/java/org/example/test01/Utils/MyUtils.java
@@ -2,7 +2,7 @@ package org.example.test01.Utils;
 
 public class MyUtils {
     public int add(int a , int b){
-        return a + b/2;
+        return a + b;
     }
     public int sub(int a ,int b){
         return a - b;

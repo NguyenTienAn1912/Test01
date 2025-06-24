0e672d4ccd0e1f9d4bda5a007cf6684e6f0b88a0
NguyenTienAn1912 <sherlockh050@gmail.com>
2025-06-25 01:00:41 +0700

    updated

--- src/test/java/org/example/test01/OnTap1/selenium_webdriver/Selenium_WebdriverTest.java
+++ src/test/java/org/example/test01/OnTap1/selenium_webdriver/Selenium_WebdriverTest.java
@@ -11,84 +11,84 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.support.ui.*;
 public class Selenium_WebdriverTest {
 
-    private WebDriver webDriver ;
-
-    @BeforeEach
-    void setUp(){
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
-    }
-
-    @Test
-    void testSteps() throws InterruptedException {
-        //Open
-        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
-
-        //click menu
-        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
-
-        //login
-        Thread.sleep(2000);
-        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
-
-        //input value to form
-        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
-        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
-        //Submit
-        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
-
-        //test
-        WebElement webElement = webDriver.findElement(By.xpath("//*[@id=\"ShoeType\"]"));
-
-        String actualFirstCategory = webElement.getText();
-        String expectedFirstCategory = "Formal Shoes";
-        assertEquals(expectedFirstCategory, actualFirstCategory);
-        webDriver.quit();
-    }
-    @Test
-    void testSteps2() throws InterruptedException {
-        //Open
-        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
-
-        //click menu
-        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
-
-        //login
-        Thread.sleep(2000);
-        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
-
-        //open register
-        webDriver.findElement(By.xpath("//*[@id=\"NewRegistration\"]")).click();
-
-        //input values to form
-        WebElement SalutationDropdown = webDriver.findElement(By.xpath("//*[@id=\"Salutation\"]"));
-        Select selectSalutation = new Select(SalutationDropdown);
-        selectSalutation.selectByVisibleText("Mr.");
-        webDriver.findElement(By.xpath("//*[@id=\"firstname\"]")).sendKeys("Nguyen Tien");
-        webDriver.findElement(By.xpath("//*[@id=\"lastname\"]")).sendKeys("An");
-        webDriver.findElement(By.xpath("//*[@id=\"emailId\"]")).sendKeys("sherlockh050@gmail.com");
-        webDriver.findElement(By.xpath("//*[@id=\"contactNumber\"]")).sendKeys("0919965342");
-        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("a19");
-        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("123");
-
-        //Submit
-        webDriver.findElement(By.xpath("//*[@id=\"first_form\"]/div/div[2]/center/input")).click();
-
-        //back to login
-        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
-
-        Thread.sleep(2000);
-        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a/li")).click();
-
-        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
-        Thread.sleep(2000);
-        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
-        //input value to form
-        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("a19");
-        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("123");
-
-        //Submit
-        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
-        webDriver.quit();
-    }
+//    private WebDriver webDriver ;
+//
+//    @BeforeEach
+//    void setUp(){
+//        WebDriverManager.chromedriver().setup();
+//        webDriver = new ChromeDriver();
+//    }
+//
+//    @Test
+//    void testSteps() throws InterruptedException {
+//        //Open
+//        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+//
+//        //click menu
+//        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+//
+//        //login
+//        Thread.sleep(2000);
+//        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
+//
+//        //input value to form
+//        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
+//        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
+//        //Submit
+//        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
+//
+//        //test
+//        WebElement webElement = webDriver.findElement(By.xpath("//*[@id=\"ShoeType\"]"));
+//
+//        String actualFirstCategory = webElement.getText();
+//        String expectedFirstCategory = "Formal Shoes";
+//        assertEquals(expectedFirstCategory, actualFirstCategory);
+//        webDriver.quit();
+//    }
+//    @Test
+//    void testSteps2() throws InterruptedException {
+//        //Open
+//        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+//
+//        //click menu
+//        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+//
+//        //login
+//        Thread.sleep(2000);
+//        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
+//
+//        //open register
+//        webDriver.findElement(By.xpath("//*[@id=\"NewRegistration\"]")).click();
+//
+//        //input values to form
+//        WebElement SalutationDropdown = webDriver.findElement(By.xpath("//*[@id=\"Salutation\"]"));
+//        Select selectSalutation = new Select(SalutationDropdown);
+//        selectSalutation.selectByVisibleText("Mr.");
+//        webDriver.findElement(By.xpath("//*[@id=\"firstname\"]")).sendKeys("Nguyen Tien");
+//        webDriver.findElement(By.xpath("//*[@id=\"lastname\"]")).sendKeys("An");
+//        webDriver.findElement(By.xpath("//*[@id=\"emailId\"]")).sendKeys("sherlockh050@gmail.com");
+//        webDriver.findElement(By.xpath("//*[@id=\"contactNumber\"]")).sendKeys("0919965342");
+//        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("a19");
+//        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("123");
+//
+//        //Submit
+//        webDriver.findElement(By.xpath("//*[@id=\"first_form\"]/div/div[2]/center/input")).click();
+//
+//        //back to login
+//        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+//
+//        Thread.sleep(2000);
+//        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a/li")).click();
+//
+//        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+//        Thread.sleep(2000);
+//        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
+//        //input value to form
+//        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("a19");
+//        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("123");
+//
+//        //Submit
+//        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
+//        webDriver.quit();
+//    }
 }
